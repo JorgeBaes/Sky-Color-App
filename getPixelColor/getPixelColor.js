f53b000e20ffe3178900eb5a5b85a577b235f707
unknown <jorgebaescaetano@gmail.com>
2020-07-20 17:30:06 -0300

    second commit

--- getPixelColor/getPixelColor.js
+++ getPixelColor/getPixelColor.js
@@ -13,8 +13,8 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-const x = 250
-const y = 600
+const x = 1
+const y = 1
 
 var color = document.querySelector('#color');
 function pick(event) {
@@ -26,36 +26,79 @@ function pick(event) {
     var rgba = 'rgba(' + data[0] + ', ' + data[1] +
     ', ' + data[2] + ', ' + (data[3] / 255) + ')';
     console.log(rgba)
-    color.style.background = rgba
-    
+    color.style.background = rgba    
 }
+
 canvas.addEventListener('click', pick);
 
+function getRGBA(cordX, cordY){
+    var pixel = ctx.getImageData(cordX, cordY, 1, 1);
+    var data = pixel.data;
+    return  'rgba(' + data[0] + ', ' + data[1] +
+        ', ' + data[2] + ', ' + (data[3] / 255) + ')';
+}
 
-function getPixelColor(imageString = 'im1.jpeg',cordX = x,cordY = y , index = 0){  
+function getPixelColor(imageString = 'im1.jpeg', arrayCord = [{ x: x, y: y, colorName: undefined }], showPixel = false, formated = false){  
     var img = new Image()
     img.src = imageString
     img.onload = function () {
         ctx.drawImage(img, 0, 0)
     }
-    setTimeout(() =>{
-        var pixel = ctx.getImageData(cordX, cordY, 1, 1);
-        var data = pixel.data;
-        var rgba = 'rgba(' + data[0] + ', ' + data[1] +
-            ', ' + data[2] + ', ' + (data[3] / 255) + ')';
-        console.log(index,rgba)      
-    },1000)
+    setTimeout(() =>{        
+        let counter = 1
+        let string = '{ time: null, '
+        arrayCord.forEach( (el , index , array ) => {
+            const rgba = getRGBA( el.x, el.y )
+            const colorN = el.colorName || `color__${counter++}`
+            const pixelPosition = `${colorN}_pixel_position: { x:${el.x}, y: ${el.y}}`
+            if(index == array.length - 1){
+                string += `${colorN}: "${rgba}"${showPixel ?` , ${pixelPosition}`:''} }`
+            }else{
+                string += `${colorN}: "${rgba}" ${showPixel ?`, ${pixelPosition}, ` : ', '}`
+            }               
+        })
+        console.log(string,formated ? ',':'')
+    },800)
 }
 
-function getAllImagesPixel(prefix = 'im', archiveType = 'jpeg', min = 1, max = 3){
-    var count = min
-    var inter_1 = setInterval(() => {
-        getPixelColor(`${prefix}${count}.${archiveType}`,undefined,undefined, count)
-        if(count == max){
-            clearInterval(inter_1)
-        }
-        count++
-    }, 1000);
+function getAllImagesPixel(prefix = 'im', archiveType = 'jpeg', min = 1, max = 3, arrayCord = [{ x: x, y: y, colorName: undefined }], showPixel = false, formated = false){
+    if(formated){
+        console.log('[')
+        var count = min
+        var inter_1 = setInterval(() => {
+            getPixelColor(`${prefix}${count}.${archiveType}`, arrayCord, showPixel, formated)
+            if(count == max){
+                clearInterval(inter_1)
+            }
+            count++
+        }, 1000);
+        setTimeout(()=>{
+            console.log(']')
+        }, 2400*(max-min))
+    }else{
+        var count = min
+        var inter_1 = setInterval(() => {
+            getPixelColor(`${prefix}${count}.${archiveType}`, arrayCord, showPixel ,formated)
+            if (count == max) {
+                clearInterval(inter_1)
+            }
+            count++
+        }, 1000);
+
+    }
 }
 
-getAllImagesPixel('im','jpeg',1,3)
+// A função getAllImagesPixel precisa de um prefixo da imagem, da extensão das imagens, do índice mínimo, do índice máximo,
+// uma lista das cordenadas dos pixels que deseja pegar a cor e dois parametros opcionais
+// showPixel true ele mostra as cordenadas dos pixels junto com as cordenadas
+// formated true console loga as cores em um array de objetos
+
+getAllImagesPixel('im','jpeg',1,3,[
+    { x: 108, y:10   },
+    { x: 100, y: 100 },
+    { x: 102, y: 102 },    
+    { x: 104, y: 103 }
+], false,true)
+
+
+
